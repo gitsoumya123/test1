df7d65a971f2962230ef67caac8a68b29111ccfd
gitsoumya123 <Soumyaranjan.B@PRO-PF05AY96.PROFINCHBLR.COM>
2019-09-22 05:55:09 -0700

    uPDATED CODE FOR PAYMENT CHECK

---
+++ src/main/java/com/profinch/Initializer/model/CustomerDetails.java
@@ -0,0 +1,13 @@
+package com.profinch.Initializer.model;
+
+import lombok.Data;
+
+@Data
+public class CustomerDetails {
+
+	private String firstName;
+	private String middleaName;
+	private String lastName;
+	private int age;
+	private String address;
+}
